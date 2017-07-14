50359f3154cba7e778bb8728ff575b389b876a4d
Rana Bhattacharyya <ranab33@gmail.com>
2017-07-14 12:35:31 -0400

    css and html

--- gameFiles/main.js
+++ gameFiles/main.js
@@ -1,9 +1,10 @@
+//cards from Byron Knoll: http://code.google.com/p/vector-playing-cards/
+
 //constructor function to establish fundamentals of game including
 //shuffling cards to randomize retults and return card by number and suit values
 function BlackJack() {
 this.suits =['diamonds', 'clubs', 'hearts', 'spades']
-this.cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-
+this.cards = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 this.randomize = function(){
   return Math.floor(Math.random()*52)
 };
@@ -14,7 +15,7 @@ this.deal = function() {
     return new Card(cardSuit, cardNumber);
   };
 
-return blackJack;
+return BlackJack;
 }
 //generating the cards and giving the cards a numerical value - face cards
 //should be worth 10 and ace would be worth either 1 or 11 depending on
@@ -24,6 +25,7 @@ return blackJack;
 function Card(number, suit) {
 this.number = number;
 this.suit = suit;
+this.card = cardNumber + ' of ' + cardSuit + '.png';
 }
 
 Card.prototype.cardShow = function() {
@@ -71,7 +73,7 @@ Hand.prototype.worth = function() {
   return total;
 };
 
-//functionality for Hit
+//Hit
 Hand.prototype.hit = function() {
   var newCard = BlackJack.deal();
     this.currentCards.push(newCard);
@@ -79,7 +81,79 @@ Hand.prototype.hit = function() {
   };
 
   return Hand;
+//==============================================================================
+//Game dealer functionality and win/loss conditions for game (behavior?)
+function Game() {
+  this.player = new hand();
+  this.dealer = new hand();
+  this.final;
+}
 
 
+//WIN LOSS CONIDTIONS
+/*  Game.prototype.status = function(stand) {
+    if (this.player.score() < 21 && this.dealer.score() < 21 && !stand) {
+      return this.final.update();
+    } if (this.dealer.score() > 21 && this.player.score() > 21) {
+      return this.final.loss('Dealer');
+    } if (this.player.score() === 21) {
+      return this.final.win('Blackjack');
+    } if (this.dealer.score() > 21 && this.player.score() < 21) {
+      return this.final.win();
+    } if (this.player.score() < 21 && this.player.score() > this.dealer.score()) {
+      return this.final.win();
+    } if (this.player.score() === this.dealer.score()) {
+      return this.final.tie();
+    }
+      return this.final.loss();
+  }; */
 
+//Dealer behavior hit till
+Game.prototype.stand = function() {
+    var dealerTotal = [];
+    while (this.dealer.score() < 17) {
+      dealerTotal.push(this.hitUser('Dealer'));
+    }
+    return results;
+  };
 
+return Game;
+
+//Messages
+function Final() {
+$(#messageSpan).text(text.replace(message));
+}
+
+  Final.prototype.update = function() {
+    return this.innerHTML().replaceWith(this.innerHTML());
+  };
+
+  Final.prototype.win = function(type) {
+    var message;
+    if (type === 'blackjack') {
+      message = this.innerHTML('Blackjack! '+this.game.player.score()
+      +'to' + this.game.dealer.score());
+    } else {
+      message = this.innerHTML('You win! ' + this.game.player.score()+'to'
+      +this.game.dealer.score());
+    }
+    return this.innerHTML().replaceWith(message);
+  };
+
+  Final.prototype.loss = function() {
+    var message = this.innerHTML('Sorry, you lose, '+this.game.player.score()
+    +'to'+this.game.dealer.score());
+    return this.innerHTML().replaceWith(message);
+  };
+
+  Final.prototype.tie = function() {
+    var message = this.innerHTML('You tied! ' + this.game.player.score()
+      +'to'+this.game.dealer.score());
+    return this.innerHTML().replaceWith(message);
+  };
+  return Final
+
+/*/add to this to initialize the game
+    this.player = new Hand();
+    this.dealer = new Hand();
+    */
