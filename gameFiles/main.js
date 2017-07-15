05db5938622957afe3b8580c67c273ed39cd6e9a
Rana Bhattacharyya <ranab33@gmail.com>
2017-07-15 12:47:43 -0400

    shuffle method changed

--- gameFiles/main.js
+++ gameFiles/main.js
@@ -1,4 +1,7 @@
 //cards from Byron Knoll: http://code.google.com/p/vector-playing-cards/
+//spoke to Rajiste Bennin about some of the logistics as we were working
+//on similar games. Raj suggested the fishcer yates shuffle. For some reason
+//my initial shuffle function would keep adding to the deck or make doubles
 
 var suits =['diamonds', 'clubs', 'hearts', 'spades'];
 var cards = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
@@ -7,43 +10,53 @@ var player = [];
 var dealer = [];
 var players = [player, dealer];
 
-
+//generates a deck of 52 cards from the suits and cards arrays above
 function createDeck() {
-  for(var i = 0; i<cards.length; i++){
-    for(var j=0; j< suits.length; j++){
-      var worth = parseInt(cards[i]);
-      if(cards[i] ===0) {
-        worth = 11;
-      } else if(worth >1 && worth < 10) {
-        worth = worth++;
-
-       } else {
+  for(var i=0;i<cards.length;i++) {
+    for(var j=0; j< suits.length; j++) {
+        var worth = parseInt(cards[i]);
+          if(cards[i] ===0) {
+          worth = 11;
+          }else if(worth > 1 && worth < 10){
+          worth = worth++;
+          } else {
           worth =10;
-          var card = {
-            Card: cards[i],
-            Suit: suits[j],
-            cardImage = 'cards/' + cards[i] + '_of_' + suits[j] + '.png'};
-          deck.push(card);
-          console.log(card);
-        }
+          }
+        var card = {
+          Card: cards[i],
+          Suit: suits[j],
+          Worth: worth,
+          Image: 'cards/' + cards[i] + '_of_' + suits[j] + '.png'};
+
+        deck.push(card);
     }
   }
 }
 
+//fischer yates shuffle as suggested by Raj
+//https://bost.ocks.org/mike/shuffle/
+function shuffle(deck) {
+  var m = deck.length, t, i;
 
-function shuffle() {
-Math.floor(Math.random()*deck.length);
+  // While there remain elements to shuffle…
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = deck[m];
+    deck[m] = deck[i];
+    deck[i] = t;
+  }
+
+  return deck;
 }
 
-function deal() {
-  shuffle();
-  for(p=0;p < 4;p++){
-    var card = deck.pop();
-    players[p].player.push(card);
-    }
-    createCard(card, p);
-    cardImage
-}
+
+//this shuffle function was not working
+//function shuffle() {
+//var cut = deck[Math.floor(Math.random()*deck.length--)];
+//var cutDeck =deck.push(cut);
+//return cutDeck;
+
+
 
 function playerScore() {
   var playerTotal =0;
@@ -90,10 +103,4 @@ function checkWin() {
   var current;
 }
 
-var createBoard = function() {
-  var cardElement = document.createElement('img');
-  cardElement.setAttribute('src', cardImage);
-  document.getElementById('playerBox').appendChild('cardElement')
-}
 
-createBoard();
